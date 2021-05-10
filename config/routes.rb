2f0c38804d475d8d77b04bee59d14c30a9ea710c
devludaley <daleyluis@gmail.com>
2021-05-10 15:23:56 -0400

    add rails routes for user login/logout/logged_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :contracts, :posts, :goals
+
+      post '/login',    to: 'sessions#create'
+      post '/logout',   to: 'sessions#destroy'
+      get '/logged_in', to: 'sessions#is_logged_in?'
+        resources :users, only: [:create, :show, :index] do 
+        resources :items, only: [:create, :show, :index, :destroy]
+    end
+    
+        resources :contracts, :posts, :goals
+        resources :users, only: [:create, :show, :index]
+
     end
   end
 end
