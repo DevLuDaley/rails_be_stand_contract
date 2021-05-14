470966b7df8643a6e59ec47161aa4929061e8464
devludaley <daleyluis@gmail.com>
2021-05-14 09:46:17 -0400

    add session and registration(commented for now) routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,28 @@
 Rails.application.routes.draw do
+  post '/users',    to: 'users#create'
+  post '/login',    to: 'users#create'
+  post '/logout',   to: 'users#destroy'
+  get '/logged_in', to: 'users#is_logged_in?'
+  # post '/logout',   to: 'sessions#destroy'
+  # get '/logged_in', to: 'sessions#is_logged_in?'
+  
   namespace :api do
     namespace :v1 do
+      # root :to => 'index#index'
 
-      post '/login',    to: 'sessions#create'
-      post '/logout',   to: 'sessions#destroy'
-      get '/logged_in', to: 'sessions#is_logged_in?'
-        resources :users, only: [:create, :show, :index] do 
-        resources :items, only: [:create, :show, :index, :destroy]
-    end
+    #     resources :users, only: [:create, :show, :index] do 
+    #     resources :items, only: [:create, :show, :index, :destroy]
+    # end
     
-        resources :contracts, :posts, :goals
-        resources :users, only: [:create, :show, :index]
+        resources :contracts, :posts, :goals, :users
+        resources :sessions, only: [:create]
+  
+        # resources :registrations, only: [:create]
+        # delete :logout, to: "sessions#logout"
+        # get :logged_in, to: "sessions#logged_in"
+        # root to: "static#home"
+        
+        # resources :users, only: [:create, :show, :index]
 
     end
   end
